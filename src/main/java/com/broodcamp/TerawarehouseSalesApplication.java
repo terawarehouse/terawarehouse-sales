f75d590af9cf348736ee5a872f259257514ede4e
Edward P. Legaspi <czetsuya@gmail.com>
2019-10-02 15:18:17 +0800

    Initial commit.

---
+++ src/main/java/com/broodcamp/TerawarehouseSalesApplication.java
@@ -0,0 +1,13 @@
+package com.broodcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerawarehouseSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerawarehouseSalesApplication.class, args);
+	}
+
+}
